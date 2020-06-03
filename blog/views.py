e78758ad99ea3cf396e89f57c9f3c71d3d44d0e9
HoangLC3 <hoanglc3@fpt.com.vn>
2020-06-03 17:57:27 +0700

    first commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+def index(request):
+    context = {"posts": Post.objects.all()}
+    return render(request, "blog/index.html", context)
+
+
+def about(request):
+    context = {"title": "About"}
+    return render(request, "blog/about.html", context)
